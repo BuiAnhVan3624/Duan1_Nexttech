73097f11e05861f8a6f81248bea06b1b6fc843bd
thealuong <luongtheanh175@gmail.com>
2025-02-23 22:21:37 +0700

    cart

--- app/Models/cart.php
+++ app/Models/cart.php
@@ -17,3 +17,22 @@ class Cart extends Model
         return $this->hasMany(CartDetail::class, 'cart_id');
     }
 }
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+    protected $table = 'carts';
+    public $fillable = [
+        'user_id'
+    ];
+
+    public function cartDetail() {
+        return $this->hasMany(CartDetail::class, 'cart_id');
+    }
+}
